b19ccca7713402e9971ba1238a49e9b2620c7895
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-05-17 13:39:02 +0200

    Oh my fish at new installation's location

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/home/likewise-open/WIWI/j_dage01/.local/share/omf"
+set -gx OMF_PATH "/home/j_dage01/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG "/home/likewise-open/WIWI/j_dage01/.config/omf"
