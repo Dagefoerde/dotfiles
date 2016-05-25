519ca2b89061227c64145bd386ab5aeec96f0def
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-05-25 15:08:30 +0200

    fish docker

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 set -gx OMF_PATH "/home/j_dage01/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/likewise-open/WIWI/j_dage01/.config/omf"
+#set -gx OMF_CONFIG "/home/j_dage01/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
@@ -10,4 +10,5 @@ source $OMF_PATH/init.fish
 # own addons
 source "$HOME/.config/fish/own_functions/aliases.fish"
 source "$HOME/.config/fish/own_functions/git.fish"
+source "$HOME/.config/fish/own_functions/docker.fish"
 
---
+++ fish/own_functions/docker.fish
@@ -0,0 +1,5 @@
+# git shortcuts
+abbr -a dco docker-compose
+
+
+
