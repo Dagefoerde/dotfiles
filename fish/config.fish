38d679359dfef391c952b4c4ac6339029c5ea8d5
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-09-02 12:06:21 +0200

    Use VI Mode; Add time to right prompt of agnoster

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ set -gx OMF_PATH "/home/j_dage01/.local/share/omf"
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
+set -g fish_key_bindings fish_vi_key_bindings
+
 # own addons
 source "$HOME/.config/fish/own_functions/aliases.fish"
 source "$HOME/.config/fish/own_functions/git.fish"
