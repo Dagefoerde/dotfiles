a9835c3fa74a8af2f6cf042ffee64c3d695c9df6
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-12-21 16:42:02 +0100

    Add timestamped git status command

--- fish/own_functions/git.fish
+++ fish/own_functions/git.fish
@@ -4,6 +4,7 @@ abbr -a gd git diff
 abbr -a gc git commit -v
 abbr -a gcm git commit -m
 abbr -a gsb git status -sb
+abbr -a gst "git status -s | while read mode file; echo \$mode \$file (stat -c \\%y \$file); end;"
 abbr -a ga git add
 abbr -a gco git checkout
 abbr -a gpr git pull --rebase
