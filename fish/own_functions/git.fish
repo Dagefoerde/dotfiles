de7a70d4ed29470a653bc3d493950ae4aef54d1a
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-04-19 21:04:56 +0200

    Add github function: Opens repo in GitHub, GitLab, or any other platform, actually.

--- fish/own_functions/git.fish
+++ fish/own_functions/git.fish
@@ -11,5 +11,26 @@ abbr -a gpr git pull --rebase
 abbr -a gl "git log --graph --pretty=format:'%G? %Cred%h %Cgreen[%ci] %Cblue<%an>%Cgreen%C(bold)%d%Creset %s %Creset'"
 abbr -a gr git rebase
 
+function github
+	if [ ! -d .git ] ;
+		echo "ERROR: This isn't a git directory"; and return (false);
+	end
 
+	set -l git_url (git config --get remote.origin.url)
+	set -l git_domain (echo $git_url | awk -v FS="(@|:)" '{print $2}')
+	set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+
+	switch $git_url
+		case 'https://*'
+			set -l repo (string replace -r ".git\$" "" -- $repo) # strip trailing .git
+			set url $git_domain/$repo/tree/$git_branch
+		case 'git@*'
+			set -l repo (string replace -r ".*:" "" -- $git_url) # strip hostname
+			set -l repo (string replace -r ".git\$" "" -- $repo) # strip trailing .git
+			set url "https://$git_domain/$repo/tree/$git_branch"
+		case '*'
+			echo "ERROR: Remote origin is invalid"; and return (false);
+	end
+	sensible-browser $url
+end
 
