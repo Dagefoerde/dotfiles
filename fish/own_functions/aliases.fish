2b05937c9f0bae20f7b2683282c05d4eedfea5b6
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-05-03 11:16:11 +0200

    make ... more usable

--- fish/own_functions/aliases.fish
+++ fish/own_functions/aliases.fish
@@ -1,6 +1,6 @@
 # Directory changing
 # alias ..="cd .." # defined elsewhere
-alias ...="cd ../.."
+abbr -a ... cd ../..
 
 # Enforce use of gradle wrapper wrapper
 # Uses an available wrapper if present; otherwise system gradle
