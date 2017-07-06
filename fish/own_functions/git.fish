3389c8df27dd5eb6f55b3dbe9d9915fe9f311e4f
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-07-06 10:48:27 +0200

    enough is enough! gs -> git status
    Who needs ghostscript? (manually, at the very least? after
    all, the gs command is only shadowed for manual commands)

--- fish/own_functions/git.fish
+++ fish/own_functions/git.fish
@@ -4,6 +4,7 @@ abbr -a gd git diff
 abbr -a gc git commit -v
 abbr -a gcf git commit -v --fixup
 abbr -a gsb git status -sb
+abbr -a gs git status
 abbr -a gst "git status -s | while read mode file; echo \$mode \$file (stat -c \\%y \$file); end;"
 abbr -a ga git add
 abbr -a gaa git add --all
