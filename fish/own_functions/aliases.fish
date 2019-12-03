bfb561f055b40ed15a4e17a641035bb817027717
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-12-03 13:33:31 +0100

    make 2nd password store accessible

--- fish/own_functions/aliases.fish
+++ fish/own_functions/aliases.fish
@@ -5,3 +5,6 @@ abbr -a ... cd ../..
 # Enforce use of gradle wrapper wrapper
 # Uses an available wrapper if present; otherwise system gradle
 alias gradle="gw"
+
+# Pass for second password store
+abbr -a zhlpass env PASSWORD_STORE_DIR=/home/j_dage01/.password-store-zhldigital pass
